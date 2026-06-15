c0b68a7d0d30a8c1ccb5216a395db4bc5686aa40
Liss_Medina <evasquezm_24_2@unc.edu.pe>
2026-06-15 11:03:02 -0500

    Ejercicio2 avance.

--- EjercicioProp.1/EjercicioProp.1/MainWindow.xaml.cs
+++ EjercicioProp.1/EjercicioProp.1/MainWindow.xaml.cs
@@ -20,5 +20,35 @@ namespace EjercicioProp._1
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if(Ingreso.Text.Length == 0)
+            {
+                MessageBox.Show("Ingrese una cantidad valida:");
+                    return;
+            }
+            double ingreso = double.Parse(Ingreso.Text);
+            double total = 0;
+            double total1 = 0;
+            double total2 = 0;
+            if(Fonavi.IsChecked == true)
+            {
+                total += ingreso * 0.08;
+                RespFonavi.Text = "s/. " + total;
+            }
+            if(Imp_Renta.IsChecked == true)
+            {
+                total1 += ingreso * 0.05;
+                RespI_Rent.Text = "S/. " + total1;
+            }
+            if (A_F_P.IsChecked == true)
+            {
+                total2 += ingreso * 0.12;
+                Total.Text = "S/. " + total2;
+            }
+            double totalPagar = total + total1 + total2;
+            Total.Text = "S/. " + totalPagar;
+        }
     }
 }
