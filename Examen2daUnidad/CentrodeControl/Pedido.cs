4cb73f1194095f2b19752aa82a509b0c29a433e9
Liss_Medina <evasquezm_24_2@unc.edu.pe>
2026-08-25 14:37:43 -0500

    Ventanas

---
+++ Examen2daUnidad/CentrodeControl/Pedido.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CentrodeControl
+{
+    class Pedido
+    {
+        public int OrderID { get; set; }
+        public string Cliente { get; set; }
+        public DateTime? OrderDate { get; set; }
+        public decimal Freight { get; set; }
+        public string ShipCountry { get; set; }
+    }
+}
+
